260052fed41b1899a1a2accc05630af1a06a54d1
Frank Rietta <frank@rietta.com>
2019-08-17 17:24:41 -0400

    Implements home page for a passing test
    This update adds the MVC components needed to make the home page requirement pass along with the Rails route to configure that page to be the root index (/) for the web application.

--- spec/system/home_page_spec.rb
+++ spec/system/home_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Home Page Marketing Content', type: :system do
   it 'homepage has the title expected by marketing' do
     visit '/'
-    expect(page).to have_text 'Welcome to ACME&reg; Barn Yard Veterinary Healthcare'
+    expect(page).to have_text 'Welcome to ACME® Barn Yard Veterinary Healthcare'
   end
 end
