4ec57bd0c67794fff6c6abd56f1baa9a9e91c082
Frank Rietta <frank@rietta.com>
2019-08-17 23:13:26 -0400

    Add data validation and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :category, presence: true
+
+  enum category: [:otc, :prescription, :restricted]
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,10 +5,61 @@ RSpec.describe Product, type: :model do
   it 'is invalid when name is blank' do
     product = Product.new(name: '')
     expect(product).to be_invalid
-    expect(product.errors).to include ['name cannot be blank']
+    expect(product.errors[:name]).to eq ["can't be blank"]
   end
-  it 'is invalid when name is longer than 255 characters'
-  it 'database returns an error when forced to save a name longer than 255 characters'
-  it 'is invalid when category is nil/NULL'
-  it 'is valid when description is over 256 characters'
+
+  it 'is invalid when name is longer than 255 characters' do
+    product = Product.new(name: 'AA' * 200)
+    expect(product).to be_invalid
+    expect(product.errors[:name]).to eq ["is too long (maximum is 255 characters)"]
+  end
+
+  it 'is invalid when category is nil/NULL' do
+    product = Product.new(name: 'test')
+    expect(product).to be_invalid
+    expect(product.errors[:category]).to eq ["can't be blank"]
+  end
+
+  it 'can be saved to the database even when description is over 256 characters' do
+    product = Product.new(
+      name: 'test',
+      category: :otc,
+      description: 'AA' * 512
+    )
+    expect do
+      product.save!
+    end.to_not raise_error
+  end
+
+  describe 'is invalid when category is not one of :otc, :presciption, or :restricted' do
+    it 'is invalid with bogus category' do
+      expect do
+        Product.new(
+          name: 'test',
+          category: 'Bogus Category Name'
+        )
+      end.to raise_error ArgumentError
+    end
+
+    it 'is valid with otc' do
+      expect do
+        Product.new(name: 'test', category: :otc)
+      end.to_not raise_error
+    end
+
+    it 'is valid with prescription' do
+      expect do
+        Product.new(name: 'test', category: :prescription)
+      end.to_not raise_error
+    end
+
+
+    it 'is valid with restricted' do
+      expect do
+        Product.new(name: 'test', category: :restricted)
+      end.to_not raise_error
+    end
+  end
+
+
 end
