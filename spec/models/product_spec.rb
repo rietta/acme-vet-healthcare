59385e054cff084a14eb5de5d97419c022d64a67
Frank Rietta <frank@rietta.com>
2019-08-17 21:36:10 -0400

    Added a failing test about the name not being blank

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is invalid when name is blank' do
+    product = Product.new(name: '')
+    expect(product).to be_invalid
+    expect(product.errors).to include ['name cannot be blank']
+  end
+  it 'is invalid when name is longer than 255 characters'
+  it 'database returns an error when forced to save a name longer than 255 characters'
+  it 'is invalid when category is nil/NULL'
+  it 'is valid when description is over 256 characters'
 end
