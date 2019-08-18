8b443ad5bb60bed646a811b02bb1b88807217e4e
Frank Rietta <frank@rietta.com>
2019-08-17 20:51:43 -0400

    Update products database migration
    After using scaffold to prototype a model, it is helpful to edit the database migration to add database constraints that allow the PostgreSQL database to help enforce data integrity. This informs PostgreSQL the following:
    
    - name, published, and category must be specified and cannot be blank.
    - published is defaulted to false
    
    These constraints are obvious from the product design. We do not have to wait until later to add them just because we are practicing TDD.

--- db/migrate/20190818004943_create_products.rb
+++ db/migrate/20190818004943_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.boolean :published
-      t.string :category
+      t.boolean :published, default: false, null: false, index: true
+      t.string :category, null: false
 
       t.timestamps
     end
