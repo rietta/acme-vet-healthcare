5d2167a1d3b8385a3dc3c0f6d97b0da8611912ef
Frank Rietta <frank@rietta.com>
2019-08-17 20:58:19 -0400

    Ran database migrations to prepare the database
    This is the result of running:
    bundle exec rake db:migrate && bundle exec rake db:test:prepare

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_08_18_004943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.boolean "published", default: false, null: false
+    t.string "category", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["published"], name: "index_products_on_published"
+  end
 
 end
