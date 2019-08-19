3c6ded4834b68b51c93021f716d5c43e06f76831
Frank Rietta <frank@rietta.com>
2019-08-19 10:33:21 -0400

    Spelling correction

--- spec/system/add_to_cart_flow_spec.rb
+++ spec/system/add_to_cart_flow_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
-# This test excercises the shopping cart as through a web browser.
+# This test exercises the shopping cart as through a web browser.
 RSpec.describe 'Shopping Cart User Experience', type: :system do
   it 'allows items to be added and removed from the cart from the product page' do
-    # I'm using Factory Bot here to create lists of random products and overridding
-    # a value so that I have an exact count known to me at this point
     product = FactoryBot.create(:product, published: true)
 
     visit product_path(product.id)
