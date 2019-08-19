19f2928a0bde5f142d5c7c15012204e3cd43929f
Frank Rietta <frank@rietta.com>
2019-08-18 20:40:57 -0400

    Adds functionality so that Product count link works
    This makes the test pass!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :category, presence: true, inclusion: { in: %w(otc prescription restricted) }
+
+  scope :published, -> { where(published: true) }
 end
