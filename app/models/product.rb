78c06d3e0b7994b590202e60c91e87789dd051b7
Frank Rietta <frank@rietta.com>
2019-08-20 18:33:10 -0400

    Add ActionText and configure it with the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
+  has_rich_text :description
+
   validates :name, presence: true, length: { maximum: 255 }
-  validates :category, presence: true, inclusion: { in: %w(otc prescription restricted) }
+  validates :category, presence: true, inclusion: { in: %w[otc prescription restricted] }
 
   scope :published, -> { where(published: true) }
 end
