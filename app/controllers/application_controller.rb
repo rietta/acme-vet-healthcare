68b8de6b72ac828dedd2d6573dc0d1e24f8554d4
Frank Rietta <frank@rietta.com>
2019-08-19 23:45:35 -0400

    Adds vet registration number to signup form and adds form links to homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
   def save_cart
     session['cart'] = @cart.to_json
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:vet_registration_number])
+  end
 end
