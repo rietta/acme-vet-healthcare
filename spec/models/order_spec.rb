fcbef02136c81d745c021de5bdd2fde96c2abf74
Frank Rietta <frank@rietta.com>
2019-08-20 22:34:55 -0400

    Failing test on striping whitespace from name and phone

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'removes stray whitespace from string fields' do
+    it 'name' do
+      order = FactoryBot.build(:order, name: ' test   ')
+      order.valid?
+      expect(order.name).to eq 'test'
+    end
+
+    it 'phone' do
+      order = FactoryBot.build(:order, phone: '770-555-1212 ')
+      order.valid?
+      expect(order.phone).to eq '770-555-1212'
+    end
+  end
+  
 end
