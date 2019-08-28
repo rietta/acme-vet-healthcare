96a4a4837eaa1e7d69e31c15bba62d72c0ceb9e8
Frank Rietta <frank@rietta.com>
2019-08-27 20:49:08 -0400

    Order factory for customer contact

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :order do
     user { nil }
-    name { "MyString" }
-    email { "MyString" }
-    phone { "MyString" }
-    address1 { "MyString" }
-    address2 { "MyString" }
-    city { "MyString" }
-    state { "MyString" }
-    zip { "MyString" }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    phone { Faker::PhoneNumber.phone_number }
+    address1 { Faker::Address.street_address }
+    address2 { 'Suite Test 172' }
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    zip { Faker::Address.zip_code }
     status { 1 }
   end
 end
